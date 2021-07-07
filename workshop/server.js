42ad9a31308ff1d7c3c962e31000acaeb9ce3af5
Hassan Hassouna <hhasona@gmail.com>
2021-07-07 10:04:53 +0300

    up1

--- workshop/server.js
+++ workshop/server.js
@@ -1,5 +1,10 @@
 const http = require("http");
 const router = require("./router");
+const exp = require('express');
+const app = exp();
 const PORT = process.env.PORT || 3000;
 const server = http.createServer(router);
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+})
 server.listen(PORT, () => console.log(`Listening at http://localhost:3000`));
