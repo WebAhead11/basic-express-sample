47256ba5f79c38565625a3d59a439ef3c0a0408c
Hassan Hassouna <hhasona@gmail.com>
2021-07-08 15:33:49 +0300

    upa

--- workshop/handlers/log_out.js
+++ workshop/handlers/log_out.js
@@ -1,13 +1,10 @@
 const path = require("path");
 
 function logoutHandler(req, res) {
-    // public directory is one level above this, so we need the ".."
     res.clearCookie('user');
-    // res.redirect('/');
-    // res.json(JSON.stringify({ success: true }));
-    // console.log(req.body);
-    const filePath = path.join(__dirname, "..", "public", "login.html");
-    res.sendFile(filePath);
+    res.redirect('/')
+        // const filePath = path.join(__dirname, "..", "public", "login.html");
+        // res.sendFile(filePath);
 
 }
 module.exports = logoutHandler;
--- workshop/public/app.js
+++ workshop/public/app.js
@@ -1,76 +1,38 @@
-// const { response } = require("express");
-
-const inp = document.getElementById('inp')
-const form = document.getElementById('form');
 const ul = document.getElementById('options');
 const result = document.getElementById('result');
 const log_out = document.getElementById('log_out');
 
 function createPerson(data) {
     ul.innerText = "";
-    const firstLi = document.createElement('li');
-    const secondLi = document.createElement('li');
-    const thirdLi = document.createElement('li');
-    const fourthLi = document.createElement('li');
-    const fifthLi = document.createElement('img');
+    const firstName = document.createElement('li');
+    const lastName = document.createElement('li');
+    const age = document.createElement('li');
+    const salary = document.createElement('li');
+    const img = document.createElement('img');
     const div = document.getElementById('result');
     div.innerHTML = '';
     const dataValues = Object.values(data)
     console.log(dataValues);
     // div.append(sorted.innerHTML);
-    div.append(firstLi)
-    firstLi.append((dataValues[0]))
-    div.append(secondLi)
-    secondLi.append((dataValues[1]))
-    div.append(thirdLi)
-    thirdLi.append((dataValues[2]))
-    div.append(fourthLi)
-    fourthLi.append((dataValues[3]))
-    div.append(fifthLi)
-    fifthLi.src = dataValues[4];
+    div.append(firstName)
+    firstName.append((dataValues[0]))
+    div.append(lastName)
+    lastName.append((dataValues[1]))
+    div.append(age)
+    age.append((dataValues[2]))
+    div.append(salary)
+    salary.append((dataValues[3]))
+    div.append(img)
+    img.src = dataValues[4];
     ul.classList.remove('show')
-        // inp.value = "";
 
 }
-const out = () => {
-    fetch('/log_out')
-        .then(response => response.json())
-        .then(data => console.log(data))
-
-    window.location.href = '/';
-}
-
-log_out.addEventListener('click', out);
+log_out.addEventListener('click', () => window.location.href = '/log_out')
 
 fetch('/autocomplete')
     .then(response => response.json())
     .then(data => {
-        ul.innerText = '';
-
-        console.log(data.length);
-
-        // // const li = document.createElement('li');
-        // const firstNameSpan = document.createElement('span');
-        // const lastNameSpan = document.createElement("span");
-
-
         createPerson(data)
-
-
-        // firstNameSpan.textContent = 'First Name: ' + data.firstName
-        // lastNameSpan.textContent = 'Last Name: ' + data.lastName;
-        // li.appendChild(firstNameSpan);
-        // li.appendChild(lastNameSpan);
-        // ul.appendChild(li);
-        // li.classList.add('selecting')
-
-        // if (inp.value === "") {
-        //     ul.classList.remove("show")
-        // } else {
-        //     ul.classList.add("show")
-        // }
     }).catch(error => {
         console.error(error);
-        response.writeHead(404, { "content-type": "text/html" });
-        response.end("<h1>Not found</h1>");
     });
